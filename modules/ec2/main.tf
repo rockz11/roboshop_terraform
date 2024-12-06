38624d1e500f296e631b2c1c104b72e8684ac1e5
Prughvi <prudhviraj3525@gmail.com>
2024-12-06 23:30:37 +0530

    m

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "instance" {
   # count deals with the list and for_each deals with the map.
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
 	Name = "${var.component_name}-${var.env}"
   }
