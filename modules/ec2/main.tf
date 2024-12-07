4c064f39e51adde94a60522fea971fd68f8e89fb
Prughvi <prudhviraj3525@gmail.com>
2024-12-07 09:35:28 +0530

    m

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
 	Name = "${var.component_name}-${var.env}"
-    target = "frontend-dev"
+    target = "catalogue-dev"
   }
   provisioner "local-exec" {
 	command = <<EOL
