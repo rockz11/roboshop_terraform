a1216fe1674cd85b09afca5d1ee48c2fbb443d81
Prughvi <prudhviraj3525@gmail.com>
2024-12-08 19:14:52 +0530

    m

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -30,15 +30,15 @@ resource "aws_instance" "instance" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-	Name = "${var.component_name}-${var.env}"
+    Name   = "${var.component_name}-${var.env}"
     target = "catalogue-dev"
   }
   provisioner "local-exec" {
-	command = <<EOL
-cd/home/ec2-user/roboshop-ansible
-ansible-playbook -i ${self.private}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} roboshop.yml
+  command = <<EOL
+   cd/home/ec2-user/roboshop-ansible
+  ansible-playbook -i ${self.private}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} roboshop.yml
   }
-}
 
+}
 
 
