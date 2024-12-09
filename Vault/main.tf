6824fa420e5224efbf47837a9b3d3e670e3e06b4
Prughvi <prudhviraj3525@gmail.com>
2024-12-09 16:39:56 +0530

    m

--- Vault/main.tf
+++ Vault/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
 provider "vault" {
   address = "https://172.31.94.86:8200"
   token = var.vault_token
@@ -10,3 +18,7 @@ data "vault_kv_secret_v2" "example" {
   name  = "My_credentials"
 }
 
+resource "local_file" "foo" {
+  content = data.vault_kv_secret_v2.example
+  filename = "/tmp/secret"
+}
